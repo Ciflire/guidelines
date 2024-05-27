b8f4400924f16652d74e1cf5d81a757b1886e5c7
VESSE Léo <leovesse@gmail.com>
2024-05-27 16:32:22 +0200

    dev template done

---
+++ flake.nix
@@ -0,0 +1,28 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      #       ↑ Swap it for your system if needed
+      #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell # .override { stdenv = pkgs.clangStdenv; } # for C dev
+          {
+            packages = with pkgs; [
+              typst
+              tinymist
+              typstyle
+            ];
+            shellHook = "";
+          };
+    };
+}
